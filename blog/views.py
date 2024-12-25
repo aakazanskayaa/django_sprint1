03f453e82f1e771472729a63fa8d4d4128440e7a
Анна Казанская <aakazanskayaa@Annas-MacBook-Pro.local>
2024-12-25 22:48:08 +0300

    яна пересдачу

--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,23 @@
 from django.shortcuts import render
 
-
 posts = [
-    {
-        'id': 0,
-        'location': 'Остров отчаянья',
-        'date': '30 сентября 1659 года',
-        'category': 'travel',
-        'text': 'Наш корабль, застигнутый в открытом море страшным штормом...',
-    },
-    {
-        'id': 1,
-        'location': 'Остров отчаянья',
-        'date': '1 октября 1659 года',
-        'category': 'not-my-day',
-        'text': 'Проснувшись поутру, я увидел, что наш корабль сняло с мели приливом...',
-    },
+    {'id': 1, 'title': 'First Post', 'content': 'Hello World!'},
+    {'id': 2, 'title': 'Second Post', 'content': 'Learning Django!'}
 ]
 
+
 def index(request):
-    return render(request, 'blog/index.html', {'posts': posts})
+    return render(request, 'index.html', {'posts': posts})
+
+
 
 def post_detail(request, id):
-    post = next((post for post in posts if post['id'] == id), None)  # Разбиваем длинную строку
-    return render(request, 'blog/detail.html', {'post': post})
+    post = next(
+        (post for post in posts if post['id'] == id),
+        None
+    )
+    return render(request, 'detail.html', {'post': post})
+
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', {'category_slug': category_slug})
+    return render(request, 'category.html', {'category_slug': category_slug})
